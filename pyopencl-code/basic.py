8c5f9a8a33ee74b043a9f9f6174db8e2cd6a7d61
mintaka33 <mintaka33@outlook.com>
2022-04-05 15:24:47 +0800

    update

---
+++ pyopencl-code/basic.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pyopencl as cl
+
+# Platform
+pf = cl.get_platforms()
+for i, p in enumerate(pf):
+    print('platform[%d]:'%i, p)
+
+p = pf[0]
+
+print(p.get_info(cl.platform_info.VERSION))
+print(p.get_info(cl.platform_info.VENDOR))
+print(p.get_info(cl.platform_info.PROFILE))
+print(p.get_info(cl.platform_info.NAME))
+
+print(p.get_info(cl.platform_info.NUMERIC_VERSION))
+print(p.get_info(cl.platform_info.HOST_TIMER_RESOLUTION))
+print(p.get_info(cl.platform_info.EXTENSIONS_WITH_VERSION))
+
+for e in p.get_info(cl.platform_info.EXTENSIONS).split():
+    print(e)
+
+# Device
+for i, p in enumerate(pf):
+    print('platform[%d]:'%i, p)
+    for t in [cl.device_type.ALL, cl.device_type.CPU, cl.device_type.GPU, cl.device_type.ACCELERATOR, cl.device_type.CUSTOM, cl.device_type.DEFAULT]:
+        print('    0x%08x:'%t, p.get_devices(device_type=cl.device_type.ALL))
+
+print('done')
